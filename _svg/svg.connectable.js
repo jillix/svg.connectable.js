174a81d577b4d7bd44b95a1113af0bb8fc9f3a6b
Ionică Bizău <bizauionica@yahoo.com>
2014-12-16 13:51:24 +0200

    Create the marker

--- _svg/svg.connectable.js
+++ _svg/svg.connectable.js
@@ -1,4 +1,7 @@
 ;(function() {
+
+    var isInited = false;
+    var marker = null;
     SVG.extend(SVG.Element, {
         connectable: function(options, elmTarget) {
 
@@ -9,6 +12,7 @@
 
             var elmSource = this;
             var line = options.container.line();
+            var markers = options.markers;
 
             // Source and target positions
             var sPos = {};
@@ -26,6 +30,27 @@
                     y2: tPos.y
                 });
             }
+
+            if (isInited === false) {
+                marker = markers.marker();
+                marker.attr({
+                    id: "triangle",
+                    viewBox: "0 0 10 10",
+                    refX: "0",
+                    refY: "5",
+                    markerUnits: "strokeWidth",
+                    markerWidth: "4",
+                    markerHeight: "5",
+                    orient: "auto"
+                });
+
+                marker.path().attr({
+                    d: "M 0 0 L 10 5 L 0 10 z"
+                });
+
+                isInited = true;
+            }
+
 
             elmSource.dragmove = updateLine;
             elmTarget.dragmove = updateLine;
