cb1b1de0fe3136452d30a93c38c1564975c39f90
Ionică Bizău <bizauionica@yahoo.com>
2014-12-28 10:51:05 +0200

    Highlight code

--- example/js/handlers.js
+++ example/js/handlers.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
-    var svg = new SVG($(".graph").get(0)).size("100%", 900);
+    var svg = new SVG($(".graph").get(0)).size("100%", 500);
     var links = svg.group();
     var markers = svg.group();
     var nodes = svg.group();
 
-    var g1 = nodes.group().translate(300, 200).draggable();
+    var g1 = nodes.group().translate(300, 100).draggable();
     g1.circle(80).fill("#C2185B");
 
-    var g2 = nodes.group().translate(100, 200).draggable();
+    var g2 = nodes.group().translate(100, 100).draggable();
     g2.circle(50).fill("#E91E63");
 
-    var g3 = nodes.group().translate(200, 400).draggable();
+    var g3 = nodes.group().translate(200, 300).draggable();
     g3.circle(100).fill("#FF5252");
 
     g1.connectable({
